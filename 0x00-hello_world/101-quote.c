80c15d30dc241cf12c6788ff17c0943080fc59e7
root <root@DESKTOP-LP5R8B0.localdomain>
2023-10-05 16:09:35 +0300

    extra2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 /**
- *  * main - Entry point
- *   * Return: Always 1 (Success)
- */
+ *  * main- Entry point
+ *   * @void: parameter
+ *    * Return: 1 (success)
+ *     **/
 int main(void)
 {
-		write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 5);
-		return (1);
+	    fprintf(stderr, "and that piece of art is useful\" - \
+			    Dora Korpar, 2015-10-19\n");
+	        return (1);
 }
+
