2ea3bbffa667429e3b9961b9e7463953cd3ca8cc
root <root@DESKTOP-LP5R8B0.localdomain>
2023-10-05 16:31:34 +0300

    extra2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-nclude <stdio.h>
+#include <stdio.h>
 /**
  *  * main- Entry point
  *   * @void: parameter
  *    * Return: 1 (success)
- *     **/
+ */
 int main(void)
 {
 	    fprintf(stderr, "and that piece of art is useful\" - \
-			    Dora Korpar, 2015-10-19\n");
-	        return (1);
+			 Dora Korpar, 2015-10-19\n");
+	return (1);
 }
