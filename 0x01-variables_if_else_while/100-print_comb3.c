69011a3ad73f170ceec9caaa99eacaec504c407b
root <root@DESKTOP-LP5R8B0.localdomain>
2023-10-06 21:58:01 +0300

    extra1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ *  *  *main - Entry point
+ *   *   *Return:  Always 0.
+ */
+int main(void)
+{
+	int x;
+	int i;
+
+	for (x = '0' ; x <= '9' ; x++)
+	{
+		for (i = '0' ; i <= '9' ; i++)
+		{
+			if (i > x)
+			{
+				putchar(x);
+				putchar(i);
+				if (x == '8' && i == '9')
+				{
+					break;
+				}	
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
