28776d188c89d6b613b6dcd5738aee3ec6d5acf4
root <root@DESKTOP-LP5R8B0.localdomain>
2023-10-10 19:50:50 +0300

    1st

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+/*
+ *  * File: holberton.h
+ *   * Auth: Brennan D Baraban
+ *    * Desc: Header file containing declarations for all functions
+ *     *       used in the 0x02-functions_nested_loops directory.
+ */
+int _putchar(char c);
+
