5df7d38d6fb1d39a80a1db322abc21acbd92c033
root <root@DESKTOP-LP5R8B0.localdomain>
2023-10-10 20:18:58 +0300

    2nd

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+ *  *  * print_alphabet - Prints "Holberton" followed by a new line
+ */
+
+
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
